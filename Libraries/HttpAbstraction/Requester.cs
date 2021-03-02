c94765000bc3ff17f4bbbfe3a1d7fddb60994547
Krzysztof Dudek <krzysztof.dudek@gaia-solutions.pl>
2021-03-02 14:16:54 +0100

    All logging to http client

--- Libraries/HttpAbstraction/Requester.cs
+++ Libraries/HttpAbstraction/Requester.cs
@@ -1,24 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using GS.DecoupleIt.Tracing;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
 using RestEase;
-#if NET5_0
-using System.Collections.Generic;
-
-#endif
 
 namespace GS.DecoupleIt.HttpAbstraction
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "AnnotationRedundancyInHierarchy")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "LogMessageIsSentenceProblem")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "TemplateIsNotCompileTimeConstantProblem")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "ConstantConditionalAccessQualifier")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "ConstantNullCoalescingCondition")]
     internal sealed class Requester : RestEase.Implementation.Requester
     {
-        public Requester([NotNull] HttpClient httpClient, [NotNull] HttpAbstractionOptions options, [NotNull] ITracer tracer) : base(httpClient)
+        public Requester(
+            [NotNull] HttpClient httpClient,
+            [NotNull] HttpAbstractionOptions options,
+            [NotNull] ITracer tracer,
+            [NotNull] ILogger<Requester> logger) : base(httpClient)
         {
             _httpClient = httpClient;
             _options    = options;
             _tracer     = tracer;
+            _logger     = logger;
         }
 
         [NotNull]
@@ -61,8 +72,16 @@ namespace GS.DecoupleIt.HttpAbstraction
             message.Headers.Add(_options.ParentSpanIdHeaderName, span.Descriptor.ParentId?.ToString());
             message.Headers.Add(_options.SpanNameHeaderName, span.Descriptor.Name);
 
+            await LogStart(message);
+
+            var stopwatch = Stopwatch.StartNew();
+
             var response = await _httpClient.SendAsync(message, completionOption, requestInfo.CancellationToken)
                                             .ConfigureAwait(false);
+
+            stopwatch.Stop();
+
+            await LogFinish(response!, stopwatch.Elapsed);
 
             if (!response.IsSuccessStatusCode && !requestInfo.AllowAnyStatusCode)
                 throw await ApiException.CreateAsync(message, response)
@@ -75,9 +94,72 @@ namespace GS.DecoupleIt.HttpAbstraction
         private readonly HttpClient _httpClient;
 
         [NotNull]
+        private readonly ILogger<Requester> _logger;
+
+        [NotNull]
         private readonly HttpAbstractionOptions _options;
 
         [NotNull]
         private readonly ITracer _tracer;
+
+        private async
+#if NETCOREAPP2_2 || NETSTANDARD2_0
+            Task
+#else
+            ValueTask
+#endif
+            LogFinish([NotNull] HttpResponseMessage context, TimeSpan duration)
+        {
+            var message = new StringBuilder(
+                "Outgoing request handling {@OperationAction} after {@OperationDuration}ms.\nStatus code: {@StatusCode}\nHeaders: {@Headers}");
+
+            var args = new List<object>
+            {
+                "finished",
+                (int) duration.TotalMilliseconds,
+                (int) context.StatusCode,
+                context.Headers?.ToDictionary(x => x.Key, x => x.Value) ?? new Dictionary<string, IEnumerable<string>>()
+            };
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+            var responseBody = context.Content is not null ? await context.Content.ReadAsStringAsync() : null;
+
+            if (!string.IsNullOrWhiteSpace(responseBody))
+            {
+                message.Append("\nBody: {@Body:l}");
+                args.Add(responseBody);
+            }
+
+            _logger.LogInformation(message.ToString(), args.ToArray());
+        }
+
+        private async
+#if NETCOREAPP2_2 || NETSTANDARD2_0
+            Task
+#else
+            ValueTask
+#endif
+            LogStart([NotNull] HttpRequestMessage context)
+        {
+            var message = new StringBuilder("Outgoing request handling {@OperationAction}.\nMethod: {@Method}\nPath: {@Path}\nHeaders: {@Headers}");
+
+            var args = new List<object>
+            {
+                "started",
+                context.Method,
+                context.RequestUri,
+                context.Headers?.ToDictionary(x => x.Key, x => x.Value) ?? new Dictionary<string, IEnumerable<string>>()
+            };
+
+            var requestBody = context.Content is not null ? await context.Content.ReadAsStringAsync() : null;
+
+            if (!string.IsNullOrWhiteSpace(requestBody))
+            {
+                message.Append("\nBody: {@Body:l}");
+                args.Add(requestBody);
+            }
+
+            _logger.LogInformation(message.ToString(), args.ToArray());
+        }
     }
 }
