c9a77ce84409ffbc272379272af573595927b9be
Krzysztof Dudek <krzysztof.dudek@gaia-solutions.pl>
2021-04-27 16:00:31 +0200

    Refresh libs

--- Samples/Samples.Documents.Command/WebHost.cs
+++ Samples/Samples.Documents.Command/WebHost.cs
@@ -1,4 +1,9 @@
 using GS.DecoupleIt.AspNetCore.Service;
+using GS.DecoupleIt.DependencyInjection.Automatic;
+using GS.DecoupleIt.HttpAbstraction;
+using GS.DecoupleIt.Options.Automatic;
+using GS.DecoupleIt.Scheduling;
+using GS.DecoupleIt.Shared;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Samples.Clients.Command.Contracts;
@@ -17,6 +22,13 @@ namespace Samples.Documents.Command
         public override void ConfigureServices(WebHostBuilderContext context, IServiceCollection serviceCollection)
         {
             base.ConfigureServices(context, serviceCollection);
+
+            var assembly = typeof(WebHost).Assembly;
+
+            serviceCollection.ScanAssemblyForImplementations(assembly);
+            serviceCollection.ScanAssemblyForOptions(assembly, context.Configuration.AsNotNull());
+            serviceCollection.ScanAssemblyForJobs(assembly);
+            serviceCollection.ScanAssemblyForHttpClients(assembly);
 
             serviceCollection.AddClientsCommandServices();
         }
